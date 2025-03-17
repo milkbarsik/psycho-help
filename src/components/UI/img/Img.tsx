e174e06399a3ebb88595467bb419b424ffc43892
Reuzer <SergeyReunin1@gmail.com>
2025-03-17 13:11:44 +0300

    Component Img

---
+++ src/components/UI/img/Img.tsx
@@ -0,0 +1,23 @@
+import { FC, useState } from "react";
+
+interface Props {
+    className: string;
+    photo?: string;
+    altPhoto: string;
+}
+
+const Img: FC<Props> = ({className, photo, altPhoto}) => {
+
+    const [imgSrc, setImgSrc] = useState(photo);
+
+    return (
+        <div>
+            <img
+            src={imgSrc}
+            onError={() => setImgSrc(altPhoto)}
+             />
+        </div>
+    )
+}
+
+export default Img;
