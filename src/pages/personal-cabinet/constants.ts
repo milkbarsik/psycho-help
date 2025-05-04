9172bddc48ab239ded8e811655747959c662954c
milkbarsik <119809394+milkbarsik@users.noreply.github.com>
2025-05-05 00:37:40 +0300

    Calendar (#15)
    * different colors of dates
    
    * tmp
    
    * FixingsConflicts12
    
    * GrandFinale
    
    * tmp
    
    * refactoring, optimization and fix problems
    
    * worker calendar
    
    ---------
    
    Co-authored-by: Reuzer <SergeyReunin1@gmail.com>

--- src/pages/personal-cabinet/constants.ts
+++ src/pages/personal-cabinet/constants.ts
@@ -1,14 +1,14 @@
 import { GetAppointment } from "@/api/types";
 
-export const appointments: GetAppointment[] = [
+export const appointmentsConsts: GetAppointment[] = [
     {
         id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
         patient_id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
         therapist_id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
         type: "Offline",
         reason: "String",
-        status: "Approved",
-        remind_time: "2025-05-02T07:59:17.799Z",
+        status: "Accepted",
+        remind_time: "2025-05-09T07:59:17.799Z",
         last_change_time: "2025-04-29T07:59:17.800Z",
         venue: "место проведения"
       },
@@ -19,7 +19,7 @@ export const appointments: GetAppointment[] = [
         type: "Offline",
         reason: "String",
         status: "Approved",
-        remind_time: "2025-05-01T07:59:17.799Z",
+        remind_time: "2025-05-05T07:59:17.799Z",
         last_change_time: "2025-05-01T07:59:17.800Z",
         venue: "место проведения"
       },
