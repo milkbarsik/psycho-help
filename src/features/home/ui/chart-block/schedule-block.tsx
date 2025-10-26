2cee8acd75d4606f266e02237b7c4e56da3f4505
Кирилл Якунин <alduin20060426@gmail.com>
2025-10-26 16:15:01 +0300

    last two sections

--- src/features/home/ui/chart-block/schedule-block.tsx
+++ src/features/home/ui/chart-block/schedule-block.tsx
@@ -6,68 +6,33 @@ import type { TextBlockProps } from '@/shared/api/types';
 
 const items: TextBlockProps[] = [
   {
-    title:
-      'Горячая линия Московской службы психологической помощи населению - 8 (495) 051 (с городского телефона) и 051 (с мобильного телефона)',
-    info: 'Режим работы: круглосуточно',
+    info:'Понедельник - Четверг',
+    title: '9:30 – 18:30 ',
   },
   {
-    title:
-      'Горячая линия по оказанию психологической помощи студенческой молодежи организованной Минобрнауки России - 8 (800) 222-55-71',
-    info: 'Режим работы: круглосуточно',
+    info:'Пятница',
+    title: '9:30 – 17:15 ',
   },
   {
-    title: 'Телефон доверия для детей, подростков и их родителей - 8 (800) 2000-122',
-    info: 'Режим работы: круглосуточно',
+    info:'Обед',
+    title: '13:00 – 13:45',
   },
-  {
-    title:
-      'Горячая линия центра экстренной психологической помощи МЧС России - 8 (495) 989-50-50 (бесплатно по Москве)',
-    info: 'Режим работы: круглосуточно',
-  },
+
+  
 ];
 
 const ChartBlock: FC = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.weekdaysWrapper}>
-        <ul className={styles.weekdaysList}>
-          <li className={styles.weekdaysElement}>
-            <p className={styles.weekdaysName}>Понедельник - Четверг</p>
-            <p className={styles.weekdaysTime}>9:30 - 18:30</p>
-          </li>
-          <li className={styles.weekdaysElement}>
-            <p className={styles.weekdaysName}>Пятница</p>
-            <p className={styles.weekdaysTime}>9:30 - 17:15</p>
-          </li>
-        </ul>
-        <div className={styles.centerSpan}>
-          <p className={styles.weekdaysDinner}>
-            <span className={styles.boldSpan}>Обед: </span>
-            <span>13:00 - 13:45</span>
-          </p>
-        </div>
+      <div className={styles.schedule}>
+        {items.map((block)=>(
+          <div className={styles.timeWrapper}>
+              <h3 className={styles.day}>{block.info}</h3>
+              <p className={styles.time}>{block.title}</p>
+          </div>
+        ))}
       </div>
-      <div className={styles.spaceBetween}>
-        <img src={lineVector} alt="line" />
-        <img src={ClockImage} alt="clock" />
-        <img src={lineVector} alt="line" />
-      </div>
-      <h3>
-        <span className={styles.detailedInformation}>
-          В выходные и праздничные дни, ночью, за срочной психологической помощью можно обратиться
-          по следующим телефонам:
-        </span>
-      </h3>
-      <div className={styles.textBlock}>
-        <ul className={styles.textList}>
-          {items.map((block, index) => (
-            <li key={index} className={styles.detailedInformationItem}>
-              <p>{block.title}</p>
-              <p className={styles.detailedInformationTime}>{block.info}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
+      {/* <img src="" alt="" /> */}
     </div>
   );
 };
--- src/features/home/ui/features-block/features-block.tsx
+++ src/features/home/ui/features-block/features-block.tsx
@@ -4,19 +4,32 @@ import styles from './features-block.module.css';
 const FeaturesBlock = () => {
   return (
     <div className={styles.featuresBlock}>
-      {FEATURES_OF_WORK.map((item) => (
-        <div key={item.title} className={styles.featureItem}>
-          <div className={styles.contentWrapper}>
-            <div className={styles.imageWrapper}>
-              <img src={item.image} alt="Иллюстрация особенностей службы" />
-            </div>
-            <div className={styles.textWrapper}>
-              <h3 className={styles.title}>{item.title}</h3>
-              <p className={styles.desc}>{item.desc}</p>
-            </div>
+      <div className={styles.container}>
+        <div className={styles.item1}>
+          <div className={styles.wrapper}>
+            <h2>Компетентые психологи</h2>
+            <p>Наши специалисты имеют высшее психологическое образование и практический опыт. Мы гарантируем бережную и профессиональную помощь</p>
           </div>
+          <a>
+            <span>Смотреть всех</span>
+          </a>
         </div>
-      ))}
+        <div className={styles.item2}>
+          <h2>График работы</h2>
+          <p>Служба доступна в течение всего года. Приём ведётся в будние дни, кроме официальных праздников</p>
+        </div>
+        <div className={styles.item3}>
+          <h2>Формат консультаций</h2>
+          <p>Очная встреча в вузе или дистанционно по телефону, через мессенджеры, Zoom, Skype или e-mail</p>
+        </div>
+        <div className={styles.item4}>
+          <div className="wrapper">
+            <h2>Перенос или пропуск <br/>консультации</h2>
+            <p>Если встречу нужно перенести, просьба сообщить об этом заранее. Пропуск без предупреждения не допускается</p>
+          </div>
+          {/* <img src="" alt="" /> */}
+          </div>
+      </div>
     </div>
   );
 };
