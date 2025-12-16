253b8d6639bf51ad1785daeb9a04c04d398205be
Artem <probkin04@mail.ru>
2025-12-16 21:26:43 +0300

    Исправление тестов

--- tests/e2e/resources.spec.ts
+++ tests/e2e/resources.spec.ts
@@ -3,10 +3,9 @@ import { test, expect } from '@playwright/test';
 test('Проверка статей', async ({ page }) => {
   await page.goto('/resources');
   const cards = page.locator('[class*=articleList] > div');
-  await expect(cards).toHaveCount(5);
+  await expect(cards).toHaveCount(6);
   await page.getByRole('button', { name: 'Показать ещё' }).click();
-  const newCount = await cards.count();
-  expect(newCount).toBeGreaterThan(5);
+  await expect(cards).toHaveCount(9);
   const navPromise = page.waitForURL(/\/article\/1/);
   await page.getByRole('button', { name: 'Читать' }).first().click();
   await navPromise;
@@ -17,10 +16,7 @@ test('Проверка тестов', async ({ page }) => {
   await page.goto('/resources');
   await page.getByRole('tab', { name: 'Тесты' }).click();
   const cards = page.getByTestId('test-card');
-  await expect(cards).toHaveCount(5);
-  await page.getByTestId('show-more-tests').click();
-  const count = await cards.count();
-  expect(count).toBeGreaterThan(5);
+  await expect(cards).toHaveCount(6);
   const firstCard = cards.first();
   const navPromise = page.waitForURL(/\/test\/.*/);
   await firstCard.click();
