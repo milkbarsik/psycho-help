4d5f8f1231947c91441808b33f3b1ec05664e17f
Bazelit <davidguamshyan777@gmail.com>
2026-03-16 13:36:52 +0300

    fixes

--- src/pages/news-page/components/telegram-banner/telegram-banner.tsx
+++ src/pages/news-page/components/telegram-banner/telegram-banner.tsx
@@ -4,12 +4,17 @@ import TelegramIcon from '@/shared/assets/images/news/telegram-icon.svg';
 
 export const TelegramBanner: React.FC = () => (
   <aside className={styles.banner}>
-    <h2 className={styles.title}>Новости в нашем Telegram</h2>
-    <p className={styles.text}>Подпишитесь, чтобы узнавать актуальную информацию самым первым</p>
+    <div className={styles.content}>
+      <h2 className={styles.title}>Новости в нашем Telegram</h2>
+      <p className={styles.text}>Подпишитесь, чтобы узнавать актуальную информацию самым первым</p>
+      <a href={'https://web.telegram.org/'} target={'_blank'} className={styles.buttonFN}>
+        Подписаться
+      </a>
+    </div>
     <div className={styles.iconWrapper}>
       <img src={TelegramIcon} alt="Telegram" />
     </div>
-    <a href={'https://web.telegram.org/'} target={'_blank'} className={styles.button}>
+    <a href={'https://web.telegram.org/'} target={'_blank'} className={styles.buttonPC}>
       Подписаться
     </a>
   </aside>
--- src/pages/news-page/news-page.tsx
+++ src/pages/news-page/news-page.tsx
@@ -5,17 +5,23 @@ import { NewsGrid } from '@/pages/news-page/components/news-grid/news-grid.tsx';
 const NewsPage = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.wrapper}>
-        <div className={styles.description}>
-          <div>
-            <h1 className={styles.title}>Новости</h1>
-            <p className={styles.subtitle}>
-              Здесь вы всегда будете в курсе последних событий, анонсов и изменений в работе нашей
-              Службы психологической помощи.
-            </p>
+      <div className={styles.hero}>
+        <div className={styles.wrapper}>
+          <div className={styles.description}>
+            <div>
+              <h1 className={styles.title}>Новости</h1>
+              <p className={styles.subtitle}>
+                Здесь вы всегда будете в курсе последних событий, анонсов и изменений в работе нашей
+                Службы психологической помощи.
+              </p>
+            </div>
+
+            <img src={psychologist_and_patient} alt="изображение психолга и пациента" />
           </div>
-          <img src={psychologist_and_patient} alt="изображение психолга и пациента" />
         </div>
+      </div>
+
+      <div className={styles.wrapper}>
         <NewsGrid />
       </div>
     </div>
