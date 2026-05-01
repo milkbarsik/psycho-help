c2b80d5107915a3b43ae8f4c43c13210dadd251c
Berlkot <91618103+Berlkot@users.noreply.github.com>
2026-05-02 02:49:02 +0300

    feat: add refresh token update mechanism (#70)

--- src/shared/api/http.ts
+++ src/shared/api/http.ts
@@ -1,23 +1,8 @@
 import axios from 'axios';
 
-// const getToken = () => {
-//   return sessionStorage.getItem('token') || localStorage.getItem('token');
-// };
-
 export const $serviceClient = axios.create({
   baseURL: import.meta.env.VITE_REACT_APP_API_URL,
   withCredentials: true,
-});
-
-$serviceClient.interceptors.request.use(async (config) => {
-  // const loginToken = getToken();
-
-  // if (loginToken) {
-  //   document.cookie = `access_token=${loginToken}; path=/`;
-  // }
-
-  // console.log(document.cookie);
-  return config;
 });
 
 export const $api = axios.create({
@@ -26,12 +11,69 @@ export const $api = axios.create({
 });
 
 $api.interceptors.request.use(async (config) => {
-  // const loginToken = getToken();
-
-  // if (loginToken) {
-  //   document.cookie = `access_token=${loginToken}; path=/`;
-  // }
-
-  // console.log(document.cookie);
   return config;
 });
+
+
+let isRefreshing = false;
+let failedQueue: { resolve: (value: unknown) => void; reject: (reason?: unknown) => void; }[] = [];
+
+const processQueue = (error: unknown, token = null) => {
+  failedQueue.forEach(prom => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+  failedQueue = [];
+};
+
+$api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      
+      if (isRefreshing) {
+        return new Promise(function(resolve, reject) {
+          failedQueue.push({ resolve, reject });
+        })
+        .then(() => {
+          return $api(originalRequest);
+        })
+        .catch(err => {
+          return Promise.reject(err);
+        });
+      }
+
+      originalRequest._retry = true;
+      isRefreshing = true;
+
+      try {
+        await axios.post(
+          `${import.meta.env.VITE_REACT_APP_API_URL}users/refresh`, 
+          {}, 
+          { withCredentials: true } 
+        );
+
+        processQueue(null);
+        return $api(originalRequest);
+
+      } catch (refreshError) {
+        processQueue(refreshError, null);
+        
+        // logout
+        window.location.href = '/';
+        
+        return Promise.reject(refreshError);
+      } finally {
+        isRefreshing = false;
+      }
+    }
+    return Promise.reject(error);
+  }
+);
