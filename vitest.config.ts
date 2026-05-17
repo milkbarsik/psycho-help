1e5dfa33ebe2e62ccae27d058961111bb85973e8
Bazelit <davidgumashyan777@gmail.com>
2026-05-17 16:06:17 +0300

    add imports

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [react(), svgr()],
